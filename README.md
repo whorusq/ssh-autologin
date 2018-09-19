62106ee38838207a384419d6ed30c38c56a196dd
whorusq <whorusq@gmail.com>
2018-09-19 20:56:50 +0800

    append the processing of the title bar

--- README.md
+++ README.md
@@ -5,13 +5,15 @@ SSH 自动登录脚本
 
 ![./screenshot.png](./screenshot.png)
 
+![./screenshot.png](./screenshot2.png)
+
 ### 如何使用当前脚本
 
 
 1. `git clone https://github.com/whorusq/shell-ssh-autologin.git`
 2. `cd shell-ssh-autologin`
 3. 修改 goto.conf ，追加服务器列表
-4. 赋予脚本可执行权限 `sudo chmod u+x goto.sh`
+4. 赋予脚本可执行权限 `sudo chmod u+x goto.sh goto.ex`
 5. 使用
 	- 方式一：`./goto.sh`
 	- 方式二：将 goto 加入当前用户全局使用
---
+++ goto.ex
@@ -0,0 +1,24 @@
+#!/usr/bin/expect
+
+set SERVER_NAME [lindex $argv 0]
+set IP [lindex $argv 1]
+set PORT [lindex $argv 2]
+set USER_NAME [lindex $argv 3]
+set PASSWORD [lindex $argv 4]
+
+
+spawn ssh -p $PORT $USER_NAME@$IP
+
+expect {
+    -timeout 300
+    "*assword" { send "$PASSWORD\r\n"; exp_continue ; sleep 3; }
+    "yes/no" { send \"yes\n\"; exp_continue; }
+    "Last*" {
+        puts "\n登录成功\n";
+        send "PROMPT_COMMAND='echo -ne \"\\033]0;$SERVER_NAME \\007\"' \r";
+        send "clear\r";
+    }
+    timeout { puts "Expect was timeout."; return }
+}
+
+interact
